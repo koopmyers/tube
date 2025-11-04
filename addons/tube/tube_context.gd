3fdba8df7a70e48ca77f6267c65cc66aea465b45
Fábio Santos <fabiosantosart@gmail.com>
2025-11-04 10:47:38 +0000

    Allow the user to customize the session ID character set; Increase characters available for the random app ID

--- addons/tube/tube_context.gd
+++ addons/tube/tube_context.gd
@@ -3,15 +3,23 @@
 class_name TubeContext extends Resource
 ## A resource that holds configuration and helper methods for managing simple multiplayer session.
 
-const _STRING_CHARACTER_SET := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+## Character set to generate app IDs. Contains most printable ASCII characters.
+const _APP_ID_CHARACTER_SET := "!#$%&()*+,-./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890:;<=>?@[]^_{|}~"
 
 @export_tool_button("Generate app id", "RandomNumberGenerator") var _generate_app_id_tool_button = (func():
-	app_id = _get_random_string(15)
+	app_id = _get_random_string(15, _APP_ID_CHARACTER_SET)
 )
 
 ## Application identifier for this multiplayer context.
 ## Must be exactly 15 characters long.
 @export var app_id: String
+
+## Character set used to generate session IDs.
+## Must not be empty and should only contain ASCII characters.
+## A larger set reduces the probability of collision. With 62 characters
+## (A–Z, a–z, 0–9), the chance of two random 5-character IDs matching is approximately 1 in 916 million.
+## For readability by players, consider removing ambiguous characters (e.g., oO0, ilj1I, z2).
+@export_multiline var session_id_characters_set: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
 
 ## List of tracker server URLs used for session signaling.
 @export var trackers_urls: Array[String] = []
@@ -35,6 +43,11 @@ func _to_string() -> String:
 
 ## Checks if the context configuration is valid.
 func is_valid() -> bool:
+	for char_index in range(session_id_characters_set.length()):
+		if session_id_characters_set.unicode_at(char_index) >= 128:
+			printerr("Session ID Character Set can only contain ASCII characters")
+			return false
+	
 	if null == app_id or 15 != app_id.length():
 		printerr("App id is invalid")
 		return false
@@ -80,23 +93,23 @@ func get_ice_servers() -> Dictionary:
 	}
 
 
-func _get_random_string(p_size: int) -> String:
+func _get_random_string(p_size: int, character_set: String) -> String:
 	var rng := RandomNumberGenerator.new()
 	rng.randomize()
 	
-	var character_set_length := _STRING_CHARACTER_SET.length()
+	var character_set_length := character_set.length()
 	
 	var out := ""
 	for i in range(p_size):
 		var index := rng.randi()%character_set_length
-		out += _STRING_CHARACTER_SET[index]
+		out += character_set[index]
 	
 	return out
 	
 
 ## Generates a random 5-character session ID.
 func generate_session_id() -> String:
-	return _get_random_string(5)
+	return _get_random_string(5, session_id_characters_set)
 
 
 ## Validates if a session ID is correct
